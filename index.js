ac0acc510c5f0a6dc188c99aeaee561e2fdf7b7b
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-23 10:30:08 -0500

    commit tree 18c5adabca4cc3b7eb69db99bc2a67321bdb0510

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+let findMatching = (drivers, string) => {
+    return drivers.filter(d => 
+        d.toLowerCase() === string.toLowerCase()
+    )
+}
+
+let fuzzyMatch = (drivers, string) => {
+    return drivers.filter(d =>
+        d.toLowerCase().charAt(0) === string.toLowerCase().charAt(0)
+        )
+}
+
+let matchName = (drivers, string) => {
+    return drivers.filter(d => 
+        d.name === string)
+}
